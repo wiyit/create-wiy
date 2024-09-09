fb0a4d142cc164050898fe4c9caee81ba4de83cd
qiezhengyuan <qiezhengyuan@wiyit.com>
2024-09-09 16:05:11 +0800

    修改项目模板

--- templates/simple/src/app.js
+++ templates/simple/src/app.js
@@ -1,12 +1,17 @@
 import wiy from '@wiyit/wiy';
 
 new wiy.App({
+    style: import('./app.css'),
     pages: {
         'page1': import('./pages/page1/page1.js'),
     },
     index: 'page1',
+    plugins: [
+        import('@wiyit/wiy-ui'),
+    ],
     lifecycle: {
         init() {
+            console.log('应用初始化成功');
         },
     },
 });   
