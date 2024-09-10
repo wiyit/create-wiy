bd8acc93cc3bc1165b64465886b8f3fb91c38af2
qiezhengyuan <qiezhengyuan@wiyit.com>
2024-09-10 16:32:36 +0800

    修改项目模板

--- templates/simple/src/pages/page1/page1.js
+++ templates/simple/src/pages/page1/page1.js
@@ -1,7 +1,7 @@
 export default {
     template: import('./page1.html'),
     components: {
-        Component1: import('../../components/component1/component1.js'),
+        'component-1': import('../../components/component1/component1.js'),
     },
     data: {
         welcomeMsg: 'Welcome to wiy!'
